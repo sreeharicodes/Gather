b1720a66c36bca76e5d1cb562f169d6672bcaf75
sreehari1997 <sreeharivijayan619@gmail.com>
2023-12-03 17:37:56 +0530

    initial commit

---
+++ utils.py
@@ -0,0 +1,217 @@
+import base64
+import requests
+from bs4 import BeautifulSoup
+from decouple import config
+
+
+URL = "https://sdma.kerala.gov.in/dam-water-level/"
+
+
+def get_soup(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return BeautifulSoup(response.text, "html.parser")
+    except requests.exceptions.RequestException:
+        return None
+
+
+def get_pdf_urls(soup):
+    urls = []
+    div = soup.find("div", class_="entry-content")
+    li_tags = div.find_all("li")
+    for li in li_tags:
+        name = li.text.strip()
+        a_tag = li.find('a', href=True)
+        href = a_tag.attrs.get("href")
+        urls.append({
+            "name": name,
+            "url": href
+        })
+
+    return urls
+
+class GitHubAPI:
+    def __init__(self):
+        self.base_url = "https://api.github.com/repos"
+        self.repo = "Gather"
+        self.owner = "sreehari1997"
+        self.token = config("ACCESS_TOKEN")
+        self.branch = "main"
+
+    def get_headers(self):
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.token),
+            'User-Agent': (
+                'Mozilla/5.0 (Windows NT 6.1; Win64; '
+                'x64; rv:47.0) Gecko/20100101 Firefox/47.0'
+            ),
+            'X-GitHub-Api-Version': '2022-11-28',
+            'Accept': 'application/vnd.github+json'
+        }
+        return headers
+
+    def get(self, url):
+        try:
+            response = requests.get(
+                url,
+                headers=self.get_headers()
+            )
+            return response.json()
+        except requests.exceptions.RequestException:
+            return None
+
+    def post(self, url, data):
+        try:
+            response = requests.post(
+                url,
+                headers=self.get_headers(),
+                data=data
+            )
+            return response.json()
+        except requests.exceptions.RequestException:
+            return None
+
+    def patch(self, url, data):
+        try:
+            response = requests.patch(
+                url,
+                headers=self.get_headers(),
+                data=data
+            )
+            return response.json()
+        except requests.exceptions.RequestException:
+            return None
+
+    def put(self, url, data):
+        try:
+            response = requests.put(
+                url,
+                headers=self.get_headers(),
+                json=data
+            )
+            return response.json()
+        except requests.exceptions.RequestException:
+            return None
+
+    def get_blob(self, url):
+        try:
+            response = requests.get(url)
+            return response.content
+        except requests.exceptions.RequestException:
+            return None
+
+    def get_last_commit(self):
+        # https://api.github.com/repos/sreehari1997/Gather/branches/main
+        url = "{}/{}/{}/branches/{}".format(
+            self.base_url,
+            self.owner,
+            self.repo,
+            self.branch
+        )
+        data = self.get(url)
+        if data:
+            return data["commit"]["sha"]
+        else:
+            return None
+
+    def create_file(self, content):
+        # This worked
+        encoded = str(base64.b64encode(content).decode("utf-8"))
+        data = {
+            "message":"test",
+            "committer":{
+                "name":"sreehari1997",
+                "email":"sreehaivijayan619@gmail.com"
+            },
+            "content": encoded
+        }
+        url = "{}/{}/{}/contents/{}".format(
+            self.base_url,
+            self.owner,
+            self.repo,
+            "2023/december/test.pdf"
+        )
+        response = self.put(url, data)
+        print(response)
+
+    def create_blob(self, content):
+        encoded = str(base64.b64encode(content).decode("utf-8"))
+        data = {
+            "content": encoded,
+            "encoding": "base64"
+        }
+        url = "{}/{}/{}/git/blobs".format(
+            self.base_url,
+            self.owner,
+            self.repo
+        )
+        response = self.post(url, data)
+        if response:
+            print(response)
+            return response["sha"]
+        else:
+            return None
+
+    def create_tree(self, commit_sha, blob_sha):
+        data = {
+          "base_tree": commit_sha,
+          "tree": [
+            {
+              "path": "2023/December/Water Levels of Main Reservoirs (KSEB) - 03/12/2023.pdf",
+              "mode": "100644",
+              "type": "blob",
+              "sha": blob_sha
+            }
+          ]
+        }
+        url = "{}/{}/{}/git/trees/".format(
+            self.base_url,
+            self.owner,
+            self.repo
+        )
+        response = self.post(url, data)
+        if response:
+            return response["sha"]
+        else:
+            return None
+
+    def create_commit(self, commit_sha, tree_sha):
+        data = {
+          "message": "Add new files at once programatically",
+          "author": {
+            "name": "sreehari1997",
+            "email": "sreeharivijayan619@gmail.com"
+          },
+          "parents": [
+            commit_sha
+          ],
+          "tree": tree_sha
+        }
+        url = "{}/{}/{}/git/commits/".format(
+            self.base_url,
+            self.owner,
+            self.repo
+        )
+        response = self.post(url, data)
+        if response:
+            return response["sha"]
+        else:
+            return None
+
+
+    def update_reference(self, commit_sha):
+        data = {
+            "sha": commit_sha
+        }
+        url = "{}/{}/{}/git/refs/heads/{}".format(
+            self.base_url,
+            self.owner,
+            self.repo,
+            self.branch
+        )
+        response = self.post(url, data)
+        if response:
+            return response
+        else:
+            return None
